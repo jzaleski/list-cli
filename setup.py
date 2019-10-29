e10b6994299676e60601808692ce88a29849f549
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2019-10-29 13:24:15 -0400

    Version 0.0.1 (pushed to _PyPi_)

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Productivity Tools',
+        'Topic :: Software Development',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
